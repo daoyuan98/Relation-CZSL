f2b8db44c6d26f5b8d3fec71f80556dec4d601f7
Ziwei XU <sylv0527@live.com>
2020-10-02 21:47:52 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -4,13 +4,14 @@ source model/misc/utils.sh
 topk=1
 ds_name="ut-zap50k"
 model_path=/path/to/model  # e.g. ./mitstates2_best/best.state
-data_path=/path/to/data
+data_path=/path/to/data  # point to the folder that contains the *folders* of mitstates or ut-zap50k
+test_batch_size=32  # reduce if OOM error occurs
 # <- end of configurations
 
 if [[ $ds_name = "ut-zap50k" ]]; then
     req_mem=2500
 else
-    req_mem=18000
+    req_mem=8000
 fi
 
 command_str="REQ_MEM=${req_mem} python eval.py \
@@ -21,6 +22,7 @@ command_str="REQ_MEM=${req_mem} python eval.py \
     --pre_feat \
     --no-pbar \
     --test_only \
+    --test_batch_size ${test_batch_size} \
     --model_path ${model_path} \
     --topk ${topk}"
 
